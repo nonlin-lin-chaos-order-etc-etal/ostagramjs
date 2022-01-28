cdbfbcf579c5151c838d0f862d6b5c7c58bce582
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2022-01-29 07:08:54 +0800

    wip

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -4,15 +4,13 @@ import querystring from 'querystring';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const req_body = req.body;
-  const map = querystring.parse(req_body)
-  axios.post("http://tranoo.com:9999/", {
+  axios.post("http://tranoo.com:9999/api/upload_into_style_transfer_type_2_nn", {
     method: "POST",
     headers: {
       'Accept': 'application/x-www-form-urlencoded',
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: querystring.stringify(map)
+    body: querystring.stringify(req.body)
   }).then(res => {
       console.log("http reply:",res)
       res.status(200).json({ response_body: res.body }); //TODO
